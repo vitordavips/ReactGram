d2f48eea3fd8c89f4142217a7710cb5c70c0aba1
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2024-11-01 17:51:22 -0300

    criando conexão com o banco  de dados

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,24 @@
 const mongoose = require("mongoose");
 
 // connection
+const dbUser = "";
+const dbPassword = "";
+
+const conn = async () => {
+    try{
+        const dbConn = await mongoose.connect(
+            `mongodb+srv://<db_username>:<db_password>@cluster0.6dpyz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+        )
+
+        console.log("Conectou ao banco!");
+        
+        return dbConn;
+
+    } catch(error) {
+        console.log(error);
+    }
+};
+
+conn();
+
+module.exports = conn;
