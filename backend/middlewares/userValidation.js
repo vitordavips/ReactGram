eb6d52ce3d909424aeeac1ff34ac88cc12b0dea2
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2024-11-07 17:14:17 -0300

    validação de criação do usuário

--- backend/middlewares/userValidation.js
+++ backend/middlewares/userValidation.js
@@ -2,9 +2,38 @@
 const {body} = require("express-validator");
 
 const userCreateValidation = () => {
-    return [body("name").isString().withMessage("O nome é obrigatorio.")];
+    // validando o nome do usuario
+    return [
+        body("name")
+            .isString()
+            .withMessage("O nome é obrigatorio.")
+            .isLength({min: 3})
+            .withMessage("O nome precisa ter no mínimo 3 caracteres."),
+        
+        body("email")
+            .isString()
+            .withMessage("O email é obrigatório.")
+            .isLength({min: 5})
+            .withMessage("O email precisa ser válido."),
+            
+        body("password")
+            .isString()
+            .withMessage("A senha é obrigatoria")
+            .isLength({min: 5})
+            .withMessage("A senha precisa ter no mínimo 5 caracteres."),
+
+        body("confirmpassword")
+            .isString()
+            .withMessage("A confirmação de senha é obrigatória.")
+            .custom((value, {req}) => {
+                if(value != req.body.password){
+                    throw new Error("As senhas não são iguais.")
+                }
+                return true;
+            })
+        ];
 };
 
 module.exports = {
     userCreateValidation,
-};
+}; 
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -7,7 +7,8 @@ const {register} = require("../controllers/useController");
 // Middlewares
 const validate = require("../middlewares/handleValidation");
 const {userCreateValidation} = require("../middlewares/userValidation");
+
 // Routes
 router.post("/register", userCreateValidation(), validate, register);
-
+ 
 module.exports = router;
