b3897f6a95dc59b10303a623a804438b79533f3b
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2025-01-07 16:22:56 -0400

    css navbar e footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,11 +18,13 @@ function App() {
     <>
       <BrowserRouter>
           <Navbar />
-          <Routes>
-              <Route path="/" element={<Home/>}/>
-              <Route path="/login" element={<Login/>}/>
-              <Route path="/register" element={<Register/>}/>
-          </Routes>
+            <div className='container'>
+              <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
+              </Routes>
+            </div>
           <Footer />
       </BrowserRouter>
     </>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,22 +2,32 @@ import './Navbar.css';
 
 //components
 import { NavLink, Link } from 'react-router-dom';
-import {BsSearch, BsHouseDoorFill, BsFillPersonaFill, BsFillCameraFill} from "react-icons/bs";
+import {BsSearch, BsHouseDoorFill, } from "react-icons/bs";
+//BsFillPersonaFill, BsFillCameraFill
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav id='nav'>
         <Link to="/">ReactGram</Link>
         <form>
             <BsSearch/>
-            <input type="text" />
+            <input type="text" placeholder='Pesquisar'/>
         </form>
         <ul id="nav-links">
-            <NavLink to="/">
-                <BsHouseDoorFill/>
-            </NavLink>
-            <NavLink to="/login">Entrar</NavLink>
-            <NavLink to="/register">Cadastrar</NavLink>
+            <li>
+                <NavLink to="/">
+                    <BsHouseDoorFill/>
+                </NavLink>
+            </li>
+
+            <li>
+                <NavLink to="/login">Entrar</NavLink>
+            </li>
+            
+            <li>
+                <NavLink to="/register">Cadastrar</NavLink>
+            </li>
+            
         </ul>
     </nav>
   )
