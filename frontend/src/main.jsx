d940d1e18a180eb6e65b64ca89edf0324ee0af4f
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2025-01-07 21:48:35 -0400

    criação do redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Redux
+import {Provider} from 'react-redux';
+import {store} from './store';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
---
+++ frontend/src/store.jsx
@@ -0,0 +1,5 @@
+import { configureStore } from "@reduzjs/toolkit";
+
+export const store = configureStore({
+    reducer:{},
+});
