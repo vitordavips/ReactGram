472dd754a1f27649e023fac8ab072be177b2b3f9
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2025-01-16 19:38:44 -0400

    criando o slice de login

--- frontend/src/services/authService.jsx
+++ frontend/src/services/authService.jsx
@@ -24,12 +24,31 @@ const register = async (data) => {
 // logout an user
 const logout = () => {
   localStorage.removeItem("user");
-}
+};
 
+//  Sing in login
+const login = async(data) => {
+  const config = requestConfig("POST", data)
+
+  try {
+    const res = await fetch(api + "/users/login", config)
+                  .then((res) => res.json())
+                  .catch((err) => err)
+    
+    if(res){
+      localStorage.setItem("user", JSON.stringify(res))
+    };
+
+    return res;
+  } catch (error) {
+    console.log(error)
+  }
+};
 
 const authService = {
     register,
     logout,
+    login
   };
 
 export default authService;
