d0efa371f99680f004fa5b829387f40dd7f3d6c4
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2025-01-20 21:51:09 -0400

    slice de perfil de usuário

--- frontend/src/slices/userSlice.jsx
+++ frontend/src/slices/userSlice.jsx
@@ -1,4 +1,5 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import userService from '../services/userService';
 
 const initialState = {
     user: {},
@@ -8,7 +9,36 @@ const initialState = {
     message: null
 };
 
-// função
+// Importa o método createAsyncThunk
+export const profile = createAsyncThunk(
+    "user/profile", // Identificador da ação
+    async(user, thunkAPI) => {
+        // obtém o token do usuário a partir do estado de autentificação
+        const token = thunkAPI.getState().auth.user.token;
+
+        // chama o serviço para buscar os dados do perfil do usuário
+        const data = await userService.profile(user, token);
+
+        return data;
+    },
+    {
+        extraReducers: (builder) => {
+            builder
+                  .addCase(profile.pending, (state) => {
+                    state.loading = true;
+                    state.error = null;
+                  })
+                  
+                  // Lida com o estado quando a ação register é concluída com sucesso
+                  .addCase(profile.fulfilled, (state, action) => {
+                    state.loading = false;
+                    state.success = true;
+                    state.error = null;
+                    state.user = action.payload;
+                  });
+        },
+    },
+);
 
 export const userSlice = createSlice({
     name: "user",
