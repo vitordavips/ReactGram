a44797350ccce35b85199305078b66b855fbae90
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2025-01-22 22:11:50 -0400

    slice de update de usuário

--- frontend/src/slices/userSlice.jsx
+++ frontend/src/slices/userSlice.jsx
@@ -35,11 +35,47 @@ export const profile = createAsyncThunk(
                     state.success = true;
                     state.error = null;
                     state.user = action.payload;
-                  });
+                  })
+
+                  .addCase(updateProfile.pending, (state) => {
+                          state.loading = true;
+                          state.error = null;
+                        })
+                        
+                        // Lida com o estado quando a ação register é concluída com sucesso
+                        .addCase(updateProfile.fulfilled, (state, action) => {
+                          state.loading = false;
+                          state.success = true;
+                          state.error = null;
+                          state.user = action.payload;
+                          state.message = "Usuário atualizado com sucesso!"
+                        })
+                        //Lida com o estado quando a ação register falha
+                        .addCase(updateProfile.rejected, (state, action) => {
+                          state.loading = false;
+                          state.error = action.payload;
+                          state.user = null;
+                        });
         },
     },
 );
 
+// Update user details
+export const updateProfile = createAsyncThunk(
+    "user/update",
+    async(user, thunkAPI) => {
+        const token = thunkAPI.getState.auth.user.token
+
+        const data = await userService.updateProfile(user, token)
+
+        // check for errors
+        if(data.errors){
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data;
+    }    
+)
 export const userSlice = createSlice({
     name: "user",
     initialState,
