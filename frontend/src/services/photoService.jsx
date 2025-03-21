19845272ae5d282a194a30b0591216ed6e615d11
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2025-03-21 16:53:29 -0400

    service para inserir fotos

--- frontend/src/services/photoService.jsx
+++ frontend/src/services/photoService.jsx
@@ -1,5 +1,18 @@
 import { api, requestConfig } from '../utils/config';
 
+//Publish an user photo
+const publishPhoto = async(data, token) => {
+    const config = requestConfig("POST", data, token, true)
+
+    try {
+        const res = await fetch(api + "/photo", config).then((res) => res.json()).catch((err) => err);
+
+        return res;
+    } catch (error) {
+        console.log(error);
+    };
+}
+
 const photoService = {};
 
 export default photoService;
