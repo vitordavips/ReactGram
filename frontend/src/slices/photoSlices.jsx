c0378cc92f4756bf6848a33453bef76e76305743
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2025-04-15 22:26:13 -0400

    slice de update da photo

--- frontend/src/slices/photoSlices.jsx
+++ frontend/src/slices/photoSlices.jsx
@@ -56,6 +56,23 @@ export const deletePhoto = createAsyncThunk(
 
         return data;
     }
+);
+
+// update a photo
+export const updatePhoto = createAsyncThunk(
+    "photo/update",
+    async(photoData, thunkAPI) => {
+        const token = thunkAPI.getState().auth.user.token;
+
+        const data = await photoService.updatePhoto({title: photoData.title}, photoData.id, token);
+
+        // check for erros
+        if(data.errors){
+            return thunkAPI.rejectWithValue(data.errors[0]);
+        }
+
+        return data;
+    }
 )
 
 export const photoSlice = createSlice({
@@ -112,6 +129,29 @@ export const photoSlice = createSlice({
                     state.loading = false;
                     state.error = action.payload;
                     state.photo = {};
+                })
+                .addCase(updatePhoto.pending, (state) => {
+                    state.loading = true;
+                    state.error = null;
+                })
+                .addCase(updatePhoto.fulfilled, (state, action) => {
+                    state.loading = false;
+                    state.success = true;
+                    state.error = null;
+
+                    state.photo.map((photo) => {
+                        if(photo._id === action.payload.photo._id){
+                            return (photo.title = action.payload.photo.title);
+                        }
+                        return photo;
+                    });
+
+                    state.message = action.payload.message;
+                })
+                .addCase(updatePhoto.rejected, (state, action) => {
+                    state.loading = false;
+                    state.error = action.payload;
+                    state.photo = null;
                 })
     },
 });
