289c97b868a45f53f783c8879c294a07e8d1fca0
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2025-04-24 15:21:34 -0400

    Concluindo o ReactGram

--- frontend/src/pages/Search/Search.jsx
+++ frontend/src/pages/Search/Search.jsx
@@ -4,7 +4,7 @@ import "./Search.css";
 import { useEffect } from "react";
 import {useSelector, useDispatch} from "react-redux";
 import {useResetComponentMessage} from "../../hooks/useResetComponentMessage";
-import {useQueyr} from "../../hooks/useQuery";
+import {useQuey} from "../../hooks/useQuery";
 
 // components
 import LikeContainer from "../../components/LikeContainer";
@@ -12,9 +12,55 @@ import PhotoItem from "../../components/PhotoItem";
 import { Link } from "react-router-dom";
 
 // redux
+import { searchPhotos, like } from "../../slices/photoSlices";
 
 const Search = () => {
-    return <div>search</div>
+    const query = useQuey();
+    const search = query.get("q");
+
+    const dispatch = useDispatch();
+
+    const resetMessage = useResetComponentMessage(dispatch);
+
+    const {user} = useSelector(state => state.auth)
+    const {photos, loading} = useSelector((state) => state.photo);
+
+    // Load photos
+    useEffect(() => {
+        dispatch(searchPhotos(search));
+    }, [dispatch, search]);
+
+    
+  // Like a photo
+  const handleLike = (photo) => {
+
+    dispatch(like(photo._id))
+
+    resetMessage()
+
+  };
+
+  if(loading) {
+    return <p>Carregando...</p>
+  }
+
+    return <div>
+        <h2>Você está procurando por: {search}</h2>
+        {photos && photos.map((photo) => (
+            <div key={photo._id}>
+                <PhotoItem photo={photo}/>
+                <LikeContainer photo={photo} user={user} handleLike={handleLike}/>
+                <Link className="btn" to={`/photos/${photo._id}`}>
+                    Ver mais
+                </Link>
+            </div>
+        ))}
+         {photos && photos.length === 0 && (
+          <h2 className="no-photos">
+            Não foram encontrados resultados para sua busca.
+          </h2>
+        )}
+    </div>
 };
 
 export default Search;
